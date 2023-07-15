af32d60f7dace31ed3de86b47a364d1215e12d94
tykay2407 <tddangkhoa240702@gmail.com>
2023-07-15 12:01:48 +0700

    khoa

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'dangkhoa.974wlgk.mongodb.net',
+    DB_USER: 'tddangkhoa240702',
+    DB_PASS: 'Khoa240702',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: '<email_user>', // Microsoft mail service
+    EMAIL_PASS: '<email_pass>',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '60000', // in milliseconds
+  };
+  module.exports = MyConstants;
